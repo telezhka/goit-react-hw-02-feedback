a6333eef7bacc0e0e8d5b705a2db31089f9e4fb6
Roman Telik <rtelik58@gmail.com>
2023-08-06 12:54:09 +0300

    add-Notification

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -2,95 +2,7 @@ import React, { Component } from 'react';
 import { Statistics } from './Statistics';
 import { Section } from './Section';
 import { FeedbackOptions } from './FeedbackOptions';
-// export class Counter extends Component {
-// static defaultProps = {
-//   step: 1,
-// };
-// state = {
-//   good: 0,
-//   neutral: 0,
-//   bad: 0,
-//   total: 0,
-//   positive: 0,
-// };
-// countTotalFeedback = () => {
-//   return new Promise(resolve => {
-//     this.setState(
-//       state => ({
-//         total: state.good + state.neutral + state.bad,
-//       }),
-//       resolve
-//     );
-//   });
-// };
-
-// countPositiveFeedbackPercentage = () => {
-//   return new Promise(resolve => {
-//     const { good, total } = this.state;
-//     const percentage = (good / total) * 100;
-//     this.setState(
-//       {
-//         positive: percentage.toFixed(1),
-//       },
-//       resolve
-//     );
-//   });
-// };
-//   handleGood = () => {
-//     this.setState(
-//       (state, props) => ({
-//         good: state.good + props.step,
-//       }),
-//       () => {
-//         this.countTotalFeedback().then(this.countPositiveFeedbackPercentage);
-//       }
-//     );
-//   };
-
-//   handleNeutral = () => {
-//     this.setState(
-//       (state, props) => ({
-//         neutral: state.neutral + props.step,
-//       }),
-//       () => {
-//         this.countTotalFeedback().then(this.countPositiveFeedbackPercentage);
-//       }
-//     );
-//   };
-//   handleBad = () => {
-//     this.setState(
-//       (state, props) => ({
-//         bad: state.bad + props.step,
-//       }),
-//       () => {
-//         this.countTotalFeedback().then(this.countPositiveFeedbackPercentage);
-//       }
-//     );
-//   };
-//   render() {
-//     const { good, neutral, bad, total, positive } = this.state;
-//     return (
-//       <div>
-//         <h1>Please leave Feedback</h1>
-//         <button type="button" onClick={this.handleGood}>
-//           Good
-//         </button>
-//         <button type="button" onClick={this.handleNeutral}>
-//           Neutral
-//         </button>
-//         <button type="button" onClick={this.handleBad}>
-//           Bad
-//         </button>
-//         <h2>Results: </h2>
-//         <span>Good: {good}</span>
-//         <span>Neutral: {neutral}</span>
-//         <span>Bad: {bad}</span>
-//         <span>Total: {total}</span>
-//         <span>Positive Feedback: {positive}%</span>
-//       </div>
-//     );
-//   }
-// }
+import { Notification } from './Notification';
 export class Counter extends Component {
   static defaultProps = {
     step: 1,
@@ -119,11 +31,11 @@ export class Counter extends Component {
   };
 
   render() {
-    let { good, neutral, bad, total, percentage } = this.state;
-    total = this.countTotalFeedback();
-    percentage = this.countPositiveFeedbackPercentage();
-
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const percentage = this.countPositiveFeedbackPercentage();
     const options = Object.keys(this.state);
+    const isFeedbackCollected = total > 0;
 
     return (
       <div>
@@ -134,15 +46,19 @@ export class Counter extends Component {
           />
         </Section>
 
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={percentage}
-          />
-        </Section>
+        {isFeedbackCollected ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={percentage}
+            />
+          </Section>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
       </div>
     );
   }
